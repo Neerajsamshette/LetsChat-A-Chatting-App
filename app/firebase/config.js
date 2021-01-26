d1ba8ceffcf8a576832a397cd04ef5c1a80cf7a2
Neerajsamshette <66945520+Neerajsamshette@users.noreply.github.com>
2021-01-26 22:00:37 +0530

    Updated config.js

--- app/firebase/config.js
+++ app/firebase/config.js
@@ -1,10 +1,8 @@
-import {} from "firebase";
-
 const firebaseConfig = {
-  apiKey: "AIzaSyCyTqd8ljxnmA4gLDvK6GqFwmK2VJ0Y_MM",
-  authDomain: "letschat-acbbb.firebaseapp.com",
-  projectId: "letschat-acbbb",
-  storageBucket: "letschat-acbbb.appspot.com",
-  messagingSenderId: "494811061861",
-  appId: "1:494811061861:web:ae69dda1dabaf11246f08c",
+  apiKey: "Your-API-Key",
+  authDomain: "Your-auth-domain",
+  projectId: "Your-project-id",
+  storageBucket: "Your-StorageBucket",
+  messagingSenderId: "Your-Messaging-ID",
+  appId: "Your-App-id",
 };
